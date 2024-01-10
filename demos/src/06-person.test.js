a9ca6c2918f0b89529ace14485f85507e982af24
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2024-01-10 16:14:13 -0300

    12-Metodologías

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -2,20 +2,31 @@ const Person = require('./06-person');
 
 describe('Test for Person Class', () => {
   let person;
+  // Arrange
   beforeEach(() => {
     person = new Person('Facundo', 50, 1.8);
   });
 
   test('should return down', () => {
+    /** AAA
+     * * Arrange / Given => Corresponde a preparar o listar escenario de pruebas
+     * * Act / when => Ejecutar o Actuar escenario
+     * * Assert / then => Resolvemos nuestras hipótesis
+     */
+
+    // Arrange
     person.weight = 50;
-    expect(person.calcIMC()).toBe('down');
+    // Act
+    const imc = person.calcIMC();
+    // Assert
+    expect(imc).toBe('down');
   });
 
   test('should return normal', () => {
     person.weight = 70;
     expect(person.calcIMC()).toBe('normal');
   });
-  
+
   test('should return overweight', () => {
     person.weight = 100;
     expect(person.calcIMC()).toBe('overweight level 2');
