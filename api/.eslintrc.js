16d6a139011294deabff10bde4ff38d5b0aaac05
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2024-01-10 22:51:46 -0300

    18-Mocking

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,17 +1,33 @@
+const rule = {
+  'no-trailing-spaces': ['error', { skipBlankLines: true }],
+  'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1, maxBOF: 0 }],
+};
+
 module.exports = {
   env: {
     browser: true,
     commonjs: true,
     es2021: true,
-    node: true,
     jest: true,
   },
-  extends: [
-    'airbnb-base',
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
+    ...rule,
   },
 };
--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,10 +1,34 @@
 const BooksService = require('./books.service');
 
+const fakeBooks = [
+  {
+    _id: 1,
+    name: 'Harry Potter y la Piedra Filosofal',
+  },
+  {
+    _id: 2,
+    name: 'Harry Potter y la Cámara de los Secretos',
+  },
+  {
+    _id: 3,
+    name: 'Harry Potter y el Prisionero de Azkaban',
+  },
+];
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => { },
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub)); // Hacemos una suplantación del archivo cuando es llamado
+
 describe('Test for BooksService', () => {
   let service;
   beforeEach(() => {
     service = new BooksService();
+    jest.clearAllMocks(); // Limpia los mocks p/c/caso prueba p/no usar los mismos valores
   });
+
   describe('Test for getBooks', () => {
     test('should return a list book', async () => {
       // Arrange
@@ -13,7 +37,17 @@ describe('Test for BooksService', () => {
       const books = await service.getBooks();
       console.log({ books });
       // Assert
-      expect(books.length).toEqual(2);
+      expect(books.length).toEqual(3);
+    });
+
+    test('should return a first book name', async () => {
+      // Arrange
+
+      // Act
+      const books = await service.getBooks();
+      console.log('book 1 ->', books[0].name);
+      // Assert
+      expect(books[0].name).toEqual('Harry Potter y la Piedra Filosofal');
     });
   });
 });
